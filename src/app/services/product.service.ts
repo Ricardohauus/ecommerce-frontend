8b3234cbd05a1a5db0425d0c7edd06bdc106960c
Ricardo Brasil <ricardohauus@gmail.com>
2020-07-29 08:25:07 -0300

    criado as funções para  buscar produto e categoria

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { ServerResponse, ProductModelServer } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,21 @@ export class ProductService {
   SERVER_URL = environment.SERVER_URL;
   constructor(private http: HttpClient) { }
   /*This is to fetch all products from the backend server*/
-  getAllProducts(numberOfResults: number = 10) {
-    return this.http.get(`${this.SERVER_URL}/products`, {
+  getAllProducts(numberOfResults: number = 10): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(`${this.SERVER_URL}/products`, {
       params: {
         limit: numberOfResults.toString()
       }
     });
+  }
+
+  /* GET SINGLE PRODUCT FROM SERVER */
+  getSingleProduct(id: number): Observable<ProductModelServer> {
+    return this.http.get<ProductModelServer>(`${this.SERVER_URL}/products/${id}`)
+  }
+
+  /* GET PRODUCT FROM ONE CATEGORY */
+  getProductsFromCategory(catName: string): Observable<ProductModelServer> {
+    return this.http.get<ProductModelServer>(`${this.SERVER_URL}/products/category/${catName}`);
   }
 }
